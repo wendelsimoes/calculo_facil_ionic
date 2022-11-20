51e6111caeb18f59405d258b82919a92412cfc3f
wendelsimoes <wendel.simoes.br@gmail.com>
2022-11-20 20:38:52 -0300

    Undefined limits complete

--- src/shared/undefined-limit-challenge.ts
+++ src/shared/undefined-limit-challenge.ts
@@ -3,14 +3,12 @@ class UndefinedLimitChallenge {
   step1: string;
   step2: string;
   answer: string;
-  to: string;
 
   constructor(challenge: string, step1: string, step2: string, answer: string, to: string) {
     this.challenge = challenge;
     this.step1 = step1;
     this.step2 = step2;
     this.answer = answer;
-    this.to = to;
   }
 }
 
