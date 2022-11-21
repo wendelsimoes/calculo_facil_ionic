fdbe4395428c7627d34317e447e8ee531e5cf3f9
wendelsimoes <wendel.simoes.br@gmail.com>
2022-11-21 13:37:09 -0300

    Add song play

--- src/app/derivative-hero/derivative-hero.component.ts
+++ src/app/derivative-hero/derivative-hero.component.ts
@@ -8,11 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class DerivativeHeroComponent implements OnInit {
 
   numbers: number[] = [];
+  songDuration: number = 0;
 
   constructor() { }
 
+  playSong() {
+    const audio = new Audio('../../assets/audio/songs/no-copy-1.mp3');
+    audio.onloadedmetadata = (e: any) => {
+      this.songDuration = e.currentTarget.duration;
+    };
+    audio.load();
+    audio.play();
+  }
+
   ngOnInit() {
     this.numbers = Array(50).fill(0).map((x, i) => i);
+    this.playSong();
   }
 
 }
